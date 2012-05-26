793a02f04b560ff2b2bc7a1529f08c2a00b5d834
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2012-05-26 23:09:11 +0400

    brefdic server components and utils
    Former-commit-id: b70fb35e9ec304c05d61bee227c077cf8c02baa9

---
+++ brefdic/brefdic-core/src/main/java/forzaverita/brefdic/model/Word.java
@@ -0,0 +1,78 @@
+package forzaverita.brefdic.model;
+
+import java.util.Set;
+
+public class Word {
+
+	private Integer id;
+	
+	private String word;
+	
+	private String description;
+	
+	private String firstLetter;
+	
+	private Set<Reference> references;
+	
+	private Set<Image> images;
+	
+	/* Methods */
+	
+	@Override
+	public String toString() {
+		return "Word [id=" + id + ", word=" + word + ", description="
+				+ description + ", firstLetter=" + firstLetter
+				+ ", references=" + references + ", images=" + images + "]";
+	}
+
+	/* Getters and Setters */
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getFirstLetter() {
+		return firstLetter;
+	}
+
+	public void setFirstLetter(String firstLetter) {
+		this.firstLetter = firstLetter;
+	}
+
+	public Set<Reference> getReferences() {
+		return references;
+	}
+
+	public void setReferences(Set<Reference> references) {
+		this.references = references;
+	}
+
+	public Set<Image> getImages() {
+		return images;
+	}
+
+	public void setImages(Set<Image> images) {
+		this.images = images;
+	}
+	
+}
---
+++ brefdic/brefdic-import/src/main/java/forzaverita/brefdic/DataImport.scala
@@ -0,0 +1,9 @@
+package forzaverita.brefdic
+
+object DataImport {
+  
+  def main(args : Array[String]) : Unit = {
+    
+  }
+  
+}
