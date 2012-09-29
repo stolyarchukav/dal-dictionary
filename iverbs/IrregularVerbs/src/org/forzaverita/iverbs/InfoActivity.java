5b5c6ca7ee58972ae2a21cdce48d42b306468eff
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2012-09-29 18:43:14 +0400

    info activity
    Former-commit-id: ced23fee7a93b3d1cf3fbaa0617ca7c6c3e21062

--- iverbs/IrregularVerbs/src/org/forzaverita/iverbs/InfoActivity.java
+++ iverbs/IrregularVerbs/src/org/forzaverita/iverbs/InfoActivity.java
@@ -1,6 +1,14 @@
 package org.forzaverita.iverbs;
 
+import org.forzaverita.iverbs.data.Constants;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
 
 public class InfoActivity extends BaseActivity {
 
@@ -9,6 +17,30 @@ public class InfoActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
         setActivityTitle();
+        
+        WebView text = (WebView) findViewById(R.id.info_usage);
+        text.loadData(getString(R.string.info_usage), "text/html", "utf-8");
     }
+    
+    public void onClickMoreApps(View view) {
+    	try {
+			startActivity(new Intent(Intent.ACTION_VIEW, 
+					Uri.parse("market://search?q=pub:ForzaVerita")));
+		}
+		catch (ActivityNotFoundException e) {
+			Log.w(Constants.LOG_TAG, "Can't open market forzaverita apps");
+		}
+	}
+    
+    public void onClickRateApp(View view) {
+    	try {
+    		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + 
+					getApplicationInfo().packageName)));
+		}
+		catch (ActivityNotFoundException e) {
+			Log.w(Constants.LOG_TAG, "Can't open market app page");
+		}
+	}
+
     
 }
