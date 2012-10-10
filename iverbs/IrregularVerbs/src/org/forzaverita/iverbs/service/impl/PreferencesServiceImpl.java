ca20bb813516b0490147444f71e36396ec4326fd
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2012-10-11 00:43:42 +0400

    statistic storing
    Former-commit-id: 867fca953538e695a09a99142b4789bcc473f11b

--- iverbs/IrregularVerbs/src/org/forzaverita/iverbs/service/impl/PreferencesServiceImpl.java
+++ iverbs/IrregularVerbs/src/org/forzaverita/iverbs/service/impl/PreferencesServiceImpl.java
@@ -1,5 +1,6 @@
 package org.forzaverita.iverbs.service.impl;
 
+import org.forzaverita.iverbs.data.Constants;
 import org.forzaverita.iverbs.data.TrainMode;
 import org.forzaverita.iverbs.data.Verb;
 import org.forzaverita.iverbs.service.PreferencesService;
@@ -7,9 +8,15 @@ import org.forzaverita.iverbs.service.PreferencesService;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class PreferencesServiceImpl implements PreferencesService {
 
+	private static final String VERB_STAT_PREFIX = "vs";
+	private static final String CORRECT = "c";
+	private static final String WRONG = "w";
+	private static final String KEY_SPLITTER = "-";
+	
 	private SharedPreferences preferences;
 	
 	public PreferencesServiceImpl(Context context) {
@@ -18,14 +25,20 @@ public class PreferencesServiceImpl implements PreferencesService {
 
 	@Override
 	public void addCorrect(int formQuest, Verb verb, TrainMode select) {
-		// TODO Auto-generated method stub
-		
+		addStat(formQuest, verb, select, CORRECT);
 	}
 
 	@Override
 	public void addWrong(int formQuest, Verb verb, TrainMode select) {
-		// TODO Auto-generated method stub
-		
+		addStat(formQuest, verb, select, WRONG);
+	}
+	
+	private void addStat(int formQuest, Verb verb, TrainMode select, String postfix) {
+		String key = VERB_STAT_PREFIX + KEY_SPLITTER + verb.getId() + KEY_SPLITTER + 
+				select.ordinal() + KEY_SPLITTER + postfix;
+		int value = preferences.getInt(key, 0);
+		preferences.edit().putInt(key, ++value).commit();
+		Log.d(Constants.LOG_TAG, formQuest + "_" + verb + "_" + select + "_" + postfix + "_" + value);
 	}
 	
 }
