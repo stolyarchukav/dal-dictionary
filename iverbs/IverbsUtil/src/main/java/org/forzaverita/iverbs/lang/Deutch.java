e9686286904b0838335aa12684146e90db7f59bd
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2012-11-22 23:33:12 +0400

    de,fr,pr
    Former-commit-id: e1f7a66fa666e56ec08e51eb2568204cd5d03113

---
+++ iverbs/IverbsUtil/src/main/java/org/forzaverita/iverbs/lang/Deutch.java
@@ -0,0 +1,10 @@
+package org.forzaverita.iverbs.lang;
+
+
+public class Deutch {
+
+	public static void main(String[] args) throws Exception {
+		LangImporter.importLang("de", 0, 4, "deutch");
+	}
+	
+}
---
+++ iverbs/IverbsUtil/src/main/java/org/forzaverita/iverbs/lang/French.java
@@ -0,0 +1,10 @@
+package org.forzaverita.iverbs.lang;
+
+
+public class French {
+
+	public static void main(String[] args) throws Exception {
+		LangImporter.importLang("fr", 0, 3, "french");
+	}
+	
+}
