7ea4c47ab416f6c489f86da675cd39052d87ec9e
Andrey Stolyarchuk <stolyarchukav@gmail.com>
2014-05-06 09:45:07 +0400

    data uploader prepare to put
    Former-commit-id: cda6e1dc7a7ed2eacf255c10b00018cf42cd1a2e

--- daldic/daldic-utils/src/main/java/org/forzaverita/daldic/util/accent/DataUpdater.java
+++ daldic/daldic-utils/src/main/java/org/forzaverita/daldic/util/accent/DataUpdater.java
@@ -1,20 +1,74 @@
 package org.forzaverita.daldic.util.accent;
 
+import org.apache.commons.lang3.StringUtils;
 import org.forzaverita.daldic.util.initial.Database;
 import org.forzaverita.daldic.util.initial.Word;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DataUpdater {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Map<String, String> words = getAccentWords();
+
+        System.out.println("Accented words: " + words.size());
+
         Database db = Database.getInstance();
         List<Integer> ids = db.getIds();
+
+        System.out.println("Word ids loaded: " + ids.size());
+
         for (Integer id : ids) {
-            Word word = db.getWord(id);
-
+            Word entity = db.getWord(id);
+            boolean modified = false;
+            for (String word : words.keySet()) {
+                if (entity.getWord().trim().equalsIgnoreCase(word)) {
+                    entity.setWord(words.get(word).toUpperCase());
+                    System.out.println("Word: " + word);
+                    modified = true;
+                }
+                String desc = entity.getDescription();
+                if (desc.startsWith(word.toUpperCase())) {
+                    entity.setDescription(desc.replaceFirst(word.toUpperCase(), words.get(word).toUpperCase()));
+                    System.out.println("Description start: " + entity.getWord() + " --- " + word);
+                    modified = true;
+                }
+                int start = desc.indexOf(" " + word + " ");
+                if (start == -1) {
+                    start = desc.indexOf(" " + word + ".");
+                }
+                if (start == -1) {
+                    start = desc.indexOf(" " + word + ",");
+                }
+                if (start != -1) {
+                    desc = desc.substring(0, start + 1) + words.get(word).toLowerCase() + desc.substring(start + word.length());
+                    entity.setDescription(desc);
+                    System.out.println("Description: " + entity.getWord() + " --- " + word + " --- " + start);
+                    modified = true;
+                }
+            }
+            if (modified) {
+                System.out.println(entity);
+            }
         }
+        db.getConnection().close();
+    }
 
+    private static Map<String, String> getAccentWords() throws IOException {
+        Map<String, String> words = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("words_result.txt"))) {
+            while (br.ready()) {
+                String word = br.readLine().toLowerCase().trim();
+                words.put(word.replace("́", ""), word);
+            }
+        }
+        return words;
     }
 
 }
