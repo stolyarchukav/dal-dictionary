ef548962f93f9e2c66e132a76075d109ac94fb16
andrey.stolyarchuk <stolyarchukav@gmail.com>
2019-02-17 17:10:08 +0300

    Menu. Go to main page

--- daldic-android/src/main/java/org/forzaverita/daldic/menu/MenuUtils.java
+++ daldic-android/src/main/java/org/forzaverita/daldic/menu/MenuUtils.java
@@ -1,40 +1,44 @@
 package org.forzaverita.daldic.menu;
 
-import org.forzaverita.daldic.R;
-import org.forzaverita.daldic.history.BookmarksActivity;
-import org.forzaverita.daldic.history.HistoryActivity;
-import org.forzaverita.daldic.preferences.AppPreferenceActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import org.forzaverita.daldic.DalDicActivity;
+import org.forzaverita.daldic.R;
+import org.forzaverita.daldic.history.BookmarksActivity;
+import org.forzaverita.daldic.history.HistoryActivity;
+import org.forzaverita.daldic.preferences.AppPreferenceActivity;
+
 public class MenuUtils {
 
 	public static boolean createOptionsMenu(Menu menu, Activity activity) {
 		MenuInflater inflater = activity.getMenuInflater();
 	    inflater.inflate(R.menu.main_menu, menu);
-	    return true;
+		return true;
 	}
 	
 	public static boolean optionsItemSelected(MenuItem item, Activity activity) {
 		switch (item.getItemId()) {
 			case R.id.menu_settings :
 				activity.startActivity(new Intent(activity, AppPreferenceActivity.class));
-				return true;
+				break;
 			case R.id.menu_seacrh :
 				activity.onSearchRequested();
-				return true;
+				break;
 			case R.id.menu_history :
 				activity.startActivity(new Intent(activity, HistoryActivity.class));
-				return true;
+				break;
 			case R.id.menu_bookmarks :
 				activity.startActivity(new Intent(activity, BookmarksActivity.class));
-			default:
-				return true;
+				break;
+			case R.id.menu_home:
+				activity.startActivity(new Intent(activity, DalDicActivity.class));
+				break;
 		}
+		return true;
 	}
 	
 }
